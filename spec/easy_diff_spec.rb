8ec3d714f8e12a40bc7b34d3caa5efe187cb7b79
abnerqian <abnerqian@yp.com>
2016-01-13 16:34:11 -0800

    Completely redid the spec to run the same tests on different types of data. Unfortunately, a lot of these new cases are failing.

--- spec/easy_diff_spec.rb
+++ spec/easy_diff_spec.rb
@@ -1,180 +1,60 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require 'yaml'
 
 describe EasyDiff do
-  before :each do
-    @original = {
-      :tags => ['a', 'b', 'c'],
-      :pos => {:x => '1', :y => '2'},
-      :some_str => "bla",
-      :some_int => 1,
-      :some_bool => false,
-      :extra_removed => "bye"
-    }
 
-    @modified = {
-      :tags => ['b', 'c', 'd'],
-      :pos => {:x => '3', :y => '2'},
-      :some_str => "bla",
-      :some_int => 2,
-      :some_bool => true,
-      :extra_added => "hi"
-    }
-
-    @removed = {
-      :tags => ['a'],
-      :pos => {:x => '1'},
-      :some_int => 1,
-      :some_bool => false,
-      :extra_removed => "bye"
-    }
-
-    @added = {
-      :tags => ['d'],
-      :pos => {:x => '3'},
-      :some_int => 2,
-      :some_bool => true,
-      :extra_added => "hi"
-    }
-  end
-  it "should compute easy_diff" do
-    removed, added = @original.easy_diff @modified
-    removed.should == @removed
-    added.should == @added
-  end
-
-  it "should compute easy_unmerge" do
-    unmerged = @modified.easy_unmerge @added
-    unmerged.should == {
-      :tags => ['b', 'c'],
-      :pos => {:y => '2'},
-      :some_str => "bla"
-    }
-  end
-
-  it "should compute easy_merge" do
-   merged = @original.easy_merge @added
-   merged.should == {
-     :tags => ['a', 'b', 'c', 'd'],
-     :pos => {:x => '3', :y => '2'},
-     :some_str => "bla",
-     :some_int => 2,
-     :some_bool => true,
-     :extra_removed => "bye",
-     :extra_added => "hi"
-   }
-   @original.should == {
-      :tags => ['a', 'b', 'c'],
-      :pos => {:x => '1', :y => '2'},
-      :some_str => "bla",
-      :some_int => 1,
-      :some_bool => false,
-      :extra_removed => "bye"
-    }
-  end
-
-  it "should stay the same" do
-    removed, added = @original.easy_diff @modified
-    unmerged = @modified.easy_unmerge added
-    original = unmerged.easy_merge removed
-    original.should == @original
+  let(:no_diffs) do
+    [{},{}]
   end
 
   it "should do a deep clone" do
-    cloned = @original.easy_clone
-    cloned.should == @original
-    cloned[:tags] << 'd'
-    cloned[:pos][:x] = '2'
-    cloned.should == {
-      :tags => ['a', 'b', 'c', 'd'],
-      :pos => {:x => '2', :y => '2'},
-      :some_str => "bla",
-      :some_int => 1,
-      :some_bool => false,
-      :extra_removed => "bye"
+    original = {
+      "pos" => {:x => 1, :y => 2}
     }
-    @original.should == {
-      :tags => ['a', 'b', 'c'],
-      :pos => {:x => '1', :y => '2'},
-      :some_str => "bla",
-      :some_int => 1,
-      :some_bool => false,
-      :extra_removed => "bye"
-    }
+    cloned = original.easy_clone
+    cloned.should == original
+
+    cloned["pos"].delete(:x)
+
+    original.easy_diff(cloned).should_not == no_diffs
   end
 
-  it "should not show empty hashes or arrays as diffs" do
-    @modified[:tags] = @original[:tags]
-    @modified[:pos] = @original[:pos]
-    @removed.delete :pos
-    @removed.delete :tags
-    @added.delete :pos
-    @added.delete :tags
-    removed, added = @original.easy_diff @modified
-    removed.should == @removed
-    added.should == @added
-  end
+  # Tests that are dependent on the inputted data. Please edit or add
+  # to the test_data.yml file if you have data that breaks the gem.
 
-  it "should show added empty hashes as a diff" do
-    @original[:empty_array] = []
-    @modified[:empty_hash] = {}
-    @removed[:empty_array] = []
-    @added[:empty_hash] = {}
-    removed, added = @original.easy_diff @modified
-    removed.should == @removed
-    added.should == @added
-  end
+  test_data_file = File.expand_path(File.dirname(__FILE__) + "/test_data.yml")
+  test_data = YAML.load_file test_data_file
 
-  it "should only check empty? on Hashes and Arrays" do
-    original = {:possibly_empty_string => ""}
-    modified = {:possibly_empty_string => "not empty"}
-    removed, added = original.easy_diff modified
-    removed.should == {:possibly_empty_string => ""}
-    added.should == {:possibly_empty_string => "not empty"}
-  end
+  test_data.each do |test_case|
+    describe "handling #{test_case["name"]}" do
+      let(:original) do
+        test_case["original"]
+      end
 
-  describe 'handling Arrays containing Hashes' do
-    let(:original) do
-      {
-        "key" => [
-          {"c" => "1"},
-          {"a" => "2"},
-          {"a" => "1"},
-        ]
-      }
-    end
+      let(:modified) do
+        test_case["modified"]
+      end
 
-    it "should merge properly" do
-      to_merge = {
-        "key" => [
-          {"b" => "2"},
-        ]
-      }
+      it "should compute easy_diff correctly" do
+        removed, added = original.easy_diff modified
 
-      merged = original.easy_merge to_merge
-      merged.should == {
-        "key" => [
-          {"a" => "1"},
-          {"a" => "2"},
-          {"b" => "2"},
-          {"c" => "1"},
-        ]
-      }
-    end
+        removed.should == test_case["expected_removed"]
+        added.should == test_case["expected_added"]
+      end
 
-    it "should unmerge properly" do
-      to_unmerge = {
-        "key" => [
-          {"a" => "2"},
-        ]
-      }
+      it "should be able to progress original hash to modified hash with no computed diffs" do
+        removed, added = original.easy_diff modified
+        progressed = original.easy_merge(added).easy_unmerge(removed)
 
-      unmerged = original.easy_unmerge to_unmerge
-      unmerged.should == {
-        "key" => [
-          {"a" => "1"},
-          {"c" => "1"},
-        ]
-      }
+        modified.easy_diff(progressed).should == no_diffs
+      end
+
+      it "should be able to reverse modified hash to original hash with no computed diffs" do
+        removed, added = original.easy_diff modified
+        reversed = modified.easy_merge(removed).easy_unmerge(added)
+
+        original.easy_diff(reversed).should == no_diffs
+      end
     end
   end
 end
