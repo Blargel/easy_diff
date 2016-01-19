c7c30b7c47d110a90e95b4bc8ef8aafb195b2ea0
abnerqian <abnerqian@yp.com>
2016-01-19 11:22:19 -0800

    Handle nils a little more intelligently.

--- lib/easy_diff/core.rb
+++ lib/easy_diff/core.rb
@@ -6,38 +6,44 @@ module EasyDiff
 
       if original.nil?
         added = modified.safe_dup
+
       elsif modified.nil?
         removed = original.safe_dup
+
       elsif original.is_a?(Hash) && modified.is_a?(Hash)
         removed = {}
         added   = {}
-        original_keys   = original.keys
-        modified_keys   = modified.keys
-        keys_in_common  = original_keys & modified_keys
-        keys_removed    = original_keys - modified_keys
-        keys_added      = modified_keys - original_keys
+
+        keys_in_common  = original.keys & modified.keys
+        keys_removed    = original.keys - modified.keys
+        keys_added      = modified.keys - original.keys
+
         keys_removed.each{ |key| removed[key] = original[key].safe_dup }
         keys_added.each{ |key| added[key] = modified[key].safe_dup }
+
         keys_in_common.each do |key|
           r, a = easy_diff original[key], modified[key]
-          removed[key] = r unless _blank?(r)
-          added[key] = a unless _blank?(a)
+          unless r.nil? && a.nil?
+            removed[key] = r unless _empty?(r)
+            added[key] = a unless _empty?(a)
+          end
         end
+
       elsif original.is_a?(Array) && modified.is_a?(Array)
         removed = original - modified
         added   = modified - original
+
       elsif original != modified
         removed   = original
         added     = modified
       end
+
       return removed, added
     end
 
     def self.easy_unmerge!(original, removed)
       if original.is_a?(Hash) && removed.is_a?(Hash)
-        original_keys  = original.keys
-        removed_keys   = removed.keys
-        keys_in_common = original_keys & removed_keys
+        keys_in_common = original.keys & removed.keys
         keys_in_common.each{ |key| original.delete(key) if easy_unmerge!(original[key], removed[key]).nil? }
       elsif original.is_a?(Array) && removed.is_a?(Array)
         original.reject!{ |e| removed.include?(e) }
@@ -71,12 +77,9 @@ module EasyDiff
       Marshal::load(Marshal.dump(original))
     end
 
-    def self._blank?(obj)
-      if obj.is_a?(Hash) || obj.is_a?(Array)
-        obj.empty?
-      else
-        obj.nil?
-      end
+    # Can't use regular empty? because that affects strings.
+    def self._empty?(obj)
+      (obj.is_a?(Hash) || obj.is_a?(Array)) && obj.empty?
     end
   end
 end
